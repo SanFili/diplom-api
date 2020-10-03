b449421302a85d5063505daa07472b555d858a54
SanFili <filimonova.alexandra@yandex.ru>
2020-10-03 18:27:32 +0300

    regexp

---
+++ regexp/urlValidation.js
@@ -0,0 +1,4 @@
+// eslint-disable-next-line no-useless-escape
+const urlValid = /^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9\-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-\/])*)?/;
+
+module.exports = urlValid;
