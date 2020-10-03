bf4cfc173acb71bed39f7f4c020bf58e190f8cf0
SanFili <filimonova.alexandra@yandex.ru>
2020-10-03 18:19:23 +0300

    защита авторизацией

--- app.js
+++ app.js
@@ -3,10 +3,18 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+const usersRouter = require('./routes/users');
+
+const cardsRouter = require('./routes/cards');
+
 mongoose.connect('mongodb://localhost:27017diplomdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
-    useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
 });
+
+app.use('/users', auth, usersRouter);
+app.use('/cards', auth, cardsRouter);
 
 app.listen(3000);
