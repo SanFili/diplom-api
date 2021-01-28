3b4c58577b0d04b7255992623b26316380ed5164
SanFili <filimonova.alexandra@yandex.ru>
2021-01-29 01:07:01 +0300

    cards owner

--- controllers/articles.js
+++ controllers/articles.js
@@ -4,7 +4,7 @@ const BadRequestError = require('../errors/bad-request-err');
 const ForbiddenError = require('../errors/forbidden-err');
 
 module.exports.getArticles = (req, res, next) => {
-  Article.find({})
+  Article.find({owner: req.user._id})
     .then((articles) => res.send({ data: articles }))
     .catch((err) => next(err));
 };
